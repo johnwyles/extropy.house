ef26ef97c0c92f275f0b434f49a58ca5e70ed37c
John Wyles <john@johnwyles.com>
2023-04-19 00:24:26 -0500

    Iniital commit of The Extropy House

---
+++ index.search.js
@@ -0,0 +1,23 @@
+var relearn_search_index = [
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "The Extropy House",
+    "uri": "/index.html"
+  }
+]
